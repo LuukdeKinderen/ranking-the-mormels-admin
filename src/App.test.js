2f6db6ef90d392f009f60d60bfdf2f215f9d3d34
Kinderen,Luuk L.H.P. de <l.dekinderen@student.fontys.nl>
2021-01-11 23:30:12 +0100

    fixed to pass tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Ranking the mormels Admin/i);
   expect(linkElement).toBeInTheDocument();
 });
